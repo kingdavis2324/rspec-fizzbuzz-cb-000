ddc911066ce1627100b8831446171efec9d47f39
Kenny Davis <kjdavis2423@gmail.com>
2018-04-30 21:06:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(num)
     "Fizz" #Go Fizz
   elsif num % 5 == 0 #if num is divisible by 5
     "Buzz" #Buzz
-  elsif num % 3 ==0 && num % 5 == 0 #if num is divisible by both 5 and 3
+  elsif num % 3 == 0 && num % 5 == 0 #if num is divisible by both 5 and 3
     "FizzBuzz" #FizzBuzz
   end
 end
